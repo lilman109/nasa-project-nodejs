dc49df74d30f0188ede2b3d6ae43fb250db13614
lilman109 <akira.yoshidasjsu@gmail.com>
2024-01-08 12:52:02 +0900

    nasa-project

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,52 @@
+const launches = new Map();
+
+let latesFlighNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latesFlighNumber++;
+  launches.set(
+    latesFlighNumber,
+    Object.assign(launch, {
+      flightNumber: latesFlighNumber,
+      customers: ["ZTM", "NASA"],
+      upcoming: true,
+      success: true,
+    }),
+  );
+}
+
+function abortLaunch(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+function launchExists(launchId) {
+  return launches.has(launchId);
+}
+
+module.exports = {
+  launches,
+  getAllLaunches,
+  addNewLaunch,
+  launchExists,
+  abortLaunch,
+};
---
+++ server/src/models/planets.model.js
@@ -0,0 +1,53 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
+
+const habitablePlanets = [];
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv"),
+    )
+      .pipe(
+        parse({
+          comment: "#", // # is the comment character in the file
+          columns: true, //
+        }),
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log("error");
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(
+          habitablePlanets.map((planet) => {
+            return planet["kepler_name"];
+          }),
+        );
+        resolve();
+      });
+  });
+}
+
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
+module.exports = {
+  loadPlanetsData,
+  getAllPlanets,
+};
