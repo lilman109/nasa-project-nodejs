6cee5c3c23f1c347f7dcbec3f5ed93b073b8e4e0
lilman109 <akira.yoshidasjsu@gmail.com>
2024-01-09 13:44:14 +0900

    connect to mongodb with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,28 @@
 const http = require("http");
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8080;
 
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGODB_PW}@nasa-project.xmddtyj.mongodb.net/?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
