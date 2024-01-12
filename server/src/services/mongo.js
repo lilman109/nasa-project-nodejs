8cc8f0dda4b88730cc73832d6be802874a03a29f
lilman109 <akira.yoshidasjsu@gmail.com>
2024-01-12 13:56:10 +0900

    made changes

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGODB_PW}@nasa-project.xmddtyj.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URL = `${process.env.MONGODB_URL}`;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
